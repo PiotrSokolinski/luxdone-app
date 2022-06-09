abdf4ab83607483dfe053073bbd0080e5043df56
Piotr Sokoliński <pettersokolinski@gmail.com>
2022-06-09 21:44:29 +0200

    Updates

---
+++ static/js/7.4c4f400c.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{193:function(n,o,t){"use strict";t.r(o);var e=t(0),a=t.n(e),d=t(3),u=t(71),r=t(47),c=t(56);o.default=function(){var n=Object(c.a)(),o=Object(d.f)();return a.a.createElement(u.a,{headerId:"notFound.header",subheaderId:"notFound.subheader",icon:{iconName:"notFound",alt:n("notFound.iconAlt")},leftButton:{messageId:"notFound.goBack",action:function(){return o(-1)}},rightButton:{messageId:"notFound.goDashboard",action:function(){return o(r.a.dashboard)}}})}}}]);
+//# sourceMappingURL=7.4c4f400c.chunk.js.map
